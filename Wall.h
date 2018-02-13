ebfc87cd7122df46f3cb50e81fd4d5e54596f654
tongtzeho <tangzihao@pku.edu.cn>
2018-02-13 21:57:32 +0800

    Delete Wall.h

--- Wall.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../Engine/Scene/Renderable.h"
-
-class Wall : public CE::Renderable<>
-{
-public:
-	Wall(const std::string &texResource, const float x1, const float z1, const float x2, const float z2);
-};
