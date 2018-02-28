a5291702b45a80febce2a4ecf713e17f7cc2fed2
tongtzeho <tangzihao@pku.edu.cn>
2018-02-28 18:19:13 +0800

    Update and rename PyRGBSensor.h to PyCameraSensor.h

---
+++ PyCameraSensor.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "ICameraSensor.h"
+
+typedef struct _PyCameraSensor // RGBSensor和DepthSensor共用_PyCameraSensor
+{
+private:
+	PyObject_HEAD
+public:
+	ICameraSensor *cameraSensor;
+	static PyObject* PyNew(PyTypeObject *type, PyObject *args, PyObject *kwds);
+	static int PyInit(_PyCameraSensor *self, PyObject *args);
+	static void PyDel(_PyCameraSensor *self);
+	static PyObject* PyStr(_PyCameraSensor *self);
+	static PyObject* PyRepr(_PyCameraSensor *self);
+	static PyObject* PyIsEnabled(_PyCameraSensor *self);
+	static PyObject* PySetEnabled(_PyCameraSensor *self, PyObject *args);
+	static PyObject* PySaveBMP(_PyCameraSensor *self, PyObject *args);
+private:
+	static PyMemberDef PyDataMembers[];
+	static PyMethodDef PyMethodMembers[];
+	static PyTypeObject PyClassInfo;
+public:
+	inline static const PyTypeObject *GetPyClassInfo() { return &PyClassInfo; }
+}PyCameraSensor;
