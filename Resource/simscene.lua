d257e1f8fe70a74b063ee3fdbe195b0579b3a6a3
tongtzeho <tangzihao@pku.edu.cn>
2018-03-31 17:22:38 +0800

    Add files via upload

--- Resource/simscene.lua
+++ Resource/simscene.lua
@@ -9,21 +9,13 @@ Text_CameraInfo = {
 	Depth = 15
 }
 
-Entity_FreeCamera = {
-	Comp = {
-		{"Camera", "1 0 1280 720 0.1 5000 1.308996939", "0.5 0.5 0.5 0.5 0 0 1 1 0 0"},
-		{"Text", "CameraInfo", "@simscene.lua:Text_CameraInfo"},
-		{"Script", "freeCamera"}
-	}
-}
-
 Light_Global = {
 	Amb = "1 1 1 1",
 	Diff = "0.7 0.7 0.7 1",
 	Spec = "0.5 0.5 0.5 16",
 	Dir = "0.5 -0.5 0.8",
 	Pos = "0 0 0",
-	Range = 70,
+	Range = 80,
 	Type = 1,
 }
 
@@ -39,9 +31,17 @@ Text_Safety = {
 	Depth = 15
 }
 
+Entity_FreeCamera = {
+	Comp = {
+		{"Camera", "1 0 1280 720 0.1 5000 1.308996939", "0.5 0.5 0.5 0.5 0 0 1 1 0 0"},
+		{"Text", "CameraInfo", "@simscene.lua:Text_CameraInfo"},
+		{"Script", "freeCamera"}
+	}
+}
+
 Entity_GlobalSystem = {
 	Child = {
-		{ Config = "@simscene.lua:Entity_FreeCamera", Name = "FreeCamera", Q = "0.5 0 0 0.866025403784", T = "0 20 -20" }
+		{ Config = "@simscene.lua:Entity_FreeCamera", Name = "FreeCamera", Q = "0.58 0 0 0.81", T = "0 54 -32" }
 	},
 	Comp = {
 		{ "Light", "@simscene.lua:Light_Global", "1" },
@@ -53,33 +53,6 @@ Entity_GlobalSystem = {
 	}
 }
 
-function PositionInCircle(radius, slice)
-	result = {}
-	theta = 0
-	dTheta = 2*math.pi/slice
-	for i = 0, slice, 1 do
-		x = radius*math.cos(theta)
-		y = 0
-		z = radius*math.sin(theta)
-		table.insert(result, tostring(x).." "..tostring(y).." "..tostring(z))
-		theta = theta + dTheta
-	end
-	return result
-end
-
-circle = PositionInCircle(32, 14)
-
-function RotationInCircle(slice)
-	math.randomseed(tostring(os.time()):reverse():sub(1, 6))
-	rotation = {}
-	for i = 0, slice, 1 do
-		table.insert(rotation, "0 "..tostring(math.random()*2).." 0 1")
-	end
-	return rotation
-end
-
-rotation = RotationInCircle(14)
-
 Scene = {
 	Splash = "@splash.png",
 	Root = {
@@ -87,34 +60,9 @@ Scene = {
 		{ Config = "@floor.lua:Entity", Name = "Floor" },
 		{ Config = "@platform.lua:Entity", Name = "Platform" },
 		{ Config = "@screen.lua:Entity", Name = "Screen" },
-		--{ Config = "@circle_wall.lua:Entity", Name = "CircleWall" },
-		--{ Config = "@building.lua:Entity", Name = "Building", S = "2.5 2.5 2.5", Q = "0 1.2 0 -1.2", T = "-14 2.5 26"},
 		{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00001", Q = "0 1 0 1", T = "-54 0 4" },
 		{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00002", Q = "0 1 0 1", T = "-19 0 7" },
 		{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00003", Q = "0 1 0 0.5", T = "34 0 11" },
 		{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00004", Q = "0 1 0 -1", T = "0 0 -23" },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00011", T = circle[1], Q = rotation[1] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00012", T = circle[2], Q = rotation[2] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00013", T = circle[3], Q = rotation[3] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00014", T = circle[4], Q = rotation[4] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00015", T = circle[5], Q = rotation[5] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00016", T = circle[6], Q = rotation[6] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00017", T = circle[7], Q = rotation[7] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00018", T = circle[8], Q = rotation[8] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00019", T = circle[9], Q = rotation[9] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00020", T = circle[10], Q = rotation[10] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00021", T = circle[11], Q = rotation[11] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00022", T = circle[12], Q = rotation[12] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00023", T = circle[13], Q = rotation[13] },
-		--{ Config = "@epuck2.lua:Entity", Name = "e-puck2_00024", T = circle[14], Q = rotation[14] },
-		--{ Config = "@box.lua:Entity", Name = "Box_1", T = "-20 0 0" },
-		--{ Config = "@box.lua:Entity", Name = "Box_2", T = "-20 0 -2" },
-		--{ Config = "@box.lua:Entity", Name = "Box_3", T = "-20 0 2" },
-		--{ Config = "@box.lua:Entity", Name = "Box_4", T = "-20 3 0" },
-		--{ Config = "@box.lua:Entity", Name = "Box_5", T = "-20 3 -2" },
-		--{ Config = "@box.lua:Entity", Name = "Box_6", T = "-20 3 2" },
-		--{ Config = "@box.lua:Entity", Name = "Box_7", T = "-20 6 0" },
-		--{ Config = "@box.lua:Entity", Name = "Box_8", T = "-20 6 -2" },
-		--{ Config = "@box.lua:Entity", Name = "Box_9", T = "-20 6 2" },
 	}
 }
