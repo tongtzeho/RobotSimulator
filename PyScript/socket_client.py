cde2b0f4d658f736aa24590027fccefc23861577
tongtzeho <tangzihao@pku.edu.cn>
2018-05-17 17:14:06 +0800

    Add files via upload

--- PyScript/socket_client.py
+++ PyScript/socket_client.py
@@ -1,11 +1,18 @@
-import socket
+import socket, struct, time
 
 if __name__ == "__main__":
 	ADDRESS = 'localhost'
 	PORT = 12001
 	socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	socketClient.connect((ADDRESS, PORT))
+	socketAddr = socketClient.recv(16)
+	socketAddr = struct.unpack("=I", socketAddr)[0]
+	print socketAddr
+	socketClient.send("import coolengine as ce\nimport robotsimulator as sim\nkinectRGB = sim.rgbSensor(ce.findEntity('Kinect').getComponent('RGBSensor'))")
 	while True:
-		sendBuf = raw_input()
-		socketClient.send(sendBuf)
+		socketClient.send("kinectRGB.getRemoteData("+str(socketAddr)+")")
+		image = socketClient.recv(2000000)
+		height, width, firstPixel = struct.unpack("=III", image[:12])
+		lastPixel = struct.unpack("=I", image[-4:])[0]
+		print time.time(), height, width, firstPixel, lastPixel
 	socketClient.close()
